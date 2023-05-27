01d33dfcd7254cfb32b0ddf7e4ae42671523a061
harishsekar1402 <srisaiharish1423@gmsil.com>
2023-05-27 11:02:49 +0530

    included mbt architecture

--- routes/todo_items.js
+++ routes/todo_items.js
@@ -4,6 +4,6 @@ const {getAllToDoItems} = require('../controls/todo_items')
 router.get("/",getAllToDoItems)
 // , (request, response) => {
 //     response.send('List of the items')
+// 
 // });
-
 module.exports = router;

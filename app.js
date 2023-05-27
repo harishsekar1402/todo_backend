b448ac3c7c4804c7826ed967121ffd10971d7fcb
harishsekar1402 <srisaiharish1423@gmsil.com>
2023-05-27 10:42:33 +0530

    added router

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("This is app.js");
+const express = require('express')
+const app = express()
+const PORT = 3500
+const todo_item = require('./routes/todo_items')
+
+app.get('/', (request, response) => {
+    response.send("Working")
+});
+
+app.use('/api/v1/todo_items', todo_item)
+
+app.listen(PORT, console.log("This server is running at http://localhost:3500"))

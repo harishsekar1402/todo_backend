c2e81deac5f77d3047698de36b8b09e81fc4276d
harishsekar1402 <srisaiharish1423@gmsil.com>
2023-05-27 11:58:36 +0530

    Added Delete method

--- controls/todo_items.js
+++ controls/todo_items.js
@@ -1,9 +1,17 @@
 const getAllToDoItems = (request, response) => {
     response.send("List of all todo items 👋👋👋");
 };
-const createNewToDoItem = (request, response) => {
-    // response.send("Create new todo item ");
-    response.json(request.body)
-}
 
-module.exports = { getAllToDoItems, createNewToDoItem };
+const createNewToDoItem = (request, response) => {
+    response.json(request.body);
+};
+
+const updateAToDoItem = (request, response) => {
+    response.send(`Updating item with id ${request.params.id}`);
+};
+
+const deleteAToDoItem = (request, response) => {
+    response.send(`Deleting item with id ${request.params.id}`);
+};
+
+module.exports = { getAllToDoItems, createNewToDoItem, updateAToDoItem, deleteAToDoItem };
--- routes/todo_items.js
+++ routes/todo_items.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const {getAllToDoItems,createNewToDoItem} = require('../controls/todo_items')
+const {getAllToDoItems,createNewToDoItem,updateAToDoItem,deleteAToDoItem} = require('../controls/todo_items')
 
 
 router.route('/').get(getAllToDoItems).post(createNewToDoItem)
+router.route('/:id').get(updateAToDoItem)
+router.route('/:id').delete(deleteAToDoItem)
 
 module.exports = router;
